d1e804700da2cc182fc5199cb1a7c9914b667296
devopsnetcom <167205266+devopsnetcom@users.noreply.github.com>
2025-11-04 16:54:57 +0530

    Update variables.tf

--- terraform-modules/network/variables.tf
+++ terraform-modules/network/variables.tf
@@ -4,3 +4,4 @@ variable "vnet_Name" { type = string }
 variable "vnet_Address" { type = string }
 variable "subnet_NameList" { type = list(string) }
 variable "subnet_AddressList" { type = list(string) }
+variable "vm_name" { type = string }
