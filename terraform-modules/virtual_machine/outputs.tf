919f5d91a33bba510d805ab212d9b05398d98bbc
Saileswar Saha <68107017+SaileswarSaha@users.noreply.github.com>
2025-11-22 00:41:29 +0530

    outputs corected

--- terraform-modules/virtual_machine/outputs.tf
+++ terraform-modules/virtual_machine/outputs.tf
@@ -1,15 +1,15 @@
 
 output "vm_id" {
   description = "The ID of the Virtual Machine"
-  value       = azurerm_virtual_machine.main.id
+  value       = azurerm_windows_virtual_machine.winvm.id
 }
 
 output "vm_ip_address" {
   description = "The Public IP Address of the Virtual Machine"
-  value       = azurerm_public_ip.main.ip_address
+  value       = azurerm_windows_virtual_machine.winvm.public_ip_address
 }
 
 output "vm_name" {
   description = "The Name of the Virtual Machine"
-  value       = azurerm_virtual_machine.main.name
+  value       = azurerm_windows_virtual_machine.winvm.name
 }
