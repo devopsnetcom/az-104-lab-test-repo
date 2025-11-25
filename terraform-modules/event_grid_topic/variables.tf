81f254af027231c67ec3b6fa09ed42df9646eab8
Saileswar Saha <68107017+SaileswarSaha@users.noreply.github.com>
2025-11-25 23:22:59 +0530

    vmip is removed as vm is using bastion pip

--- terraform-modules/event_grid_topic/variables.tf
+++ terraform-modules/event_grid_topic/variables.tf
@@ -9,6 +9,5 @@ variable "tenant_id" { type = string }
 variable "vm_id" { type = string }
 variable "vm_username" { type = string }
 variable "vm_password" { type = string }
-variable "vm_ip" { type = string }
 variable "bastion_name" { type = string }
 variable "bastion_id" { type = string }
